9692ddb216c97b4c6a446f9adcba94c0e0572cfb
Lucien2714 <lucien2714@gmail.com>
2021-08-19 23:57:53 +0800

    Update player.java
    updated player  superpowers' CD

--- src/main/java/club/lucien2714/hungergamesspeedrun/GameStatus/player.java
+++ src/main/java/club/lucien2714/hungergamesspeedrun/GameStatus/player.java
@@ -2,8 +2,23 @@ package club.lucien2714.hungergamesspeedrun.GameStatus;
 
 import org.bukkit.entity.Player;
 
+import java.sql.Timestamp;
+
 public class player {
     public Player p;
     public int career;
     public boolean ready;
+    public Timestamp cd;
+    public boolean resetCD()
+    {
+        this.cd=new Timestamp(System.currentTimeMillis());
+        return true;
+    }
+    public long AbilityCD(){
+        Timestamp temp=new Timestamp(System.currentTimeMillis());
+        return temp.getTime()-this.cd.getTime();
+    }
+    public boolean useAbility(){
+        this.p.
+    }
 }
